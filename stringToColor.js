1b37d2245ab68f9cf4868e30c3f26a5af6690758
dickinsonjl <dickinsonjl@gmail.com>
2013-10-28 14:57:24 +0000

    Add greyscale mode and lightness option

--- stringToColor.js
+++ stringToColor.js
@@ -1,11 +1,16 @@
-function colorCode(str)
+function colorCode(str, mode)
 {
 	var r = 128;
 	var g = 64;
 	var b = 32;
+	var gs = 0;
+
+	var lightness = 210; // min 0, max 255.
 
 	var front;
 	var back;
+
+	var output;
 
 	if(str)
 	{
@@ -23,17 +28,28 @@ function colorCode(str)
 	g = (g >= 0) ? g : -1*g;
 	b = (b >= 0) ? b : -1*b;
 
-	r = (r + g + 32) % 256;
-	g = (g + b + 64) % 256;
-	b = (b + r + 128) % 256;
+	r = (r + g + 32) % (256 - lightness) + lightness;
+	g = (g + b + 64) % (256 - lightness) + lightness;
+	b = (b + r + 128) % (256 - lightness) + lightness;
+	gs = (r + g + b) / 3;
 
 	r = r.toString(16);
 	g = g.toString(16);
 	b = b.toString(16);
+	gs = gs.toString(16);
 
 	if(r.length === 1) r = '0'+r;
 	if(g.length === 1) g = '0'+g;
 	if(b.length === 1) b = '0'+b;
+	if(gs.length === 1) gs = '0'+gs;
 
-	return ''+r+g+b;
+
+	if(mode === 'greyscale')
+	{
+		output = ''+gs+gs+gs;
+	} else {
+		output = ''+r+g+b;
+	}
+
+	return output;
 }
